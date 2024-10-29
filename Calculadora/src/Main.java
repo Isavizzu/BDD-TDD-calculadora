2b7dc9e96fc7e8a47471680ce93db736464b9f9e
Maria Fernanda <moreira.mafe@gmail.com>
2024-10-29 11:18:54 -0300

    Update Main.java

--- Calculadora/src/Main.java
+++ Calculadora/src/Main.java
@@ -2,8 +2,26 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println(calcularSubtracao(10, 20));
+        int num_1 = 10;
+        int num_2 = 20;
+        //calcularSoma(num_1, num_2);
+        // calcularSubtracao(num_1, num_2);
+        // System.out.println(calcularSubtracao(10, 20));
     }
+
+    public static int calcularSoma (int n, int num){
+        int result = n + num;
+        return result;
+    }
+
+    // public static int calcularSoma(int n, int num){
+        //return n + num;
+   // }
+
+   // public static int calcularSubtracao (int n, int num){
+       // int result = n - num;
+      //  return result;
+    //}
 
     public static int calcularSubtracao(int n, int num){
        return n - num;
